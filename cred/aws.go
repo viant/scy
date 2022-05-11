21bfefc8f3e294088ca6e3a559960b3188fcc4d4
awitas <awitas@vindicotech.com>
2022-05-10 19:18:09 -0700

    updated cred.Aws

--- cred/aws.go
+++ cred/aws.go
@@ -2,6 +2,7 @@ package cred
 
 //Aws represents AWS credentials
 type Aws struct {
+	Id     string `json:",omitempty"`
 	Key    string `json:",omitempty"`
 	Secret string `json:",omitempty"`
 	Region string `json:",omitempty"`
