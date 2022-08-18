4d06184f4d7375c56250ebf711b49a7658efde04
awitas <awitas@vindicotech.com>
2022-08-17 19:50:28 -0700

    rename uid to user_id in jwt token

--- auth/jwt/claims.go
+++ auth/jwt/claims.go
@@ -9,7 +9,7 @@ import (
 //Claims represents JWT claim
 type Claims struct {
 	Email         string      `json:"email,omitempty"`
-	UserID        int         `json:"uid,omitempty"`
+	UserID        int         `json:"user_id,omitempty"`
 	FirstName     string      `json:"first_name,omitempty"`
 	LastName      string      `json:"last_name,omitempty"`
 	AccountName   string      `json:"account_name,omitempty"`
